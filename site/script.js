70155e9e0f3caeb538ca9c8ab91e635f08569d4d
Melanie Richards <mr@melanie-richards.com>
2021-09-19 15:27:54 -0700

    Add basic styles

--- site/script.js
+++ site/script.js
@@ -10,12 +10,20 @@ document.addEventListener('DOMContentLoaded', () => {
   // Retrieve random photo from API
   const fetchPhoto = async function (delay) {
     const response = await fetch('/.netlify/functions/photos').then(response => response.json());
+    const photoColor = JSON.stringify(response.color).replace(/"/g, '');
+    const jumbleParts = document.querySelectorAll('.jumble *');
+    responseImage.style.backgroundColor = photoColor;
     responseImage.setAttribute('src', JSON.stringify(response.photoUrl).replace(/"/g, ''));
     responseImage.setAttribute('alt', JSON.stringify(response.photoAlt).replace(/"/g, ''));
     responseLink.setAttribute('href', JSON.stringify(response.photoPage).replace(/"/g, ''));
     responseAuthor.innerText = JSON.stringify(response.authorName).replace(/"/g, '');
     responseAuthor.setAttribute('href', JSON.stringify(response.authorPage).replace(/"/g, ''));
-    setTimeout(() => {responseContainer.classList.add('reference-container--shown')}, delay);
+    setTimeout(() => {
+      responseContainer.classList.add('reference-container--shown');
+      for (let i = 0; i < jumbleParts.length; i++) {
+        jumbleParts[i].style.fill = photoColor;
+      }
+    }, delay);
   };
 
   // Show photo on first fetch
